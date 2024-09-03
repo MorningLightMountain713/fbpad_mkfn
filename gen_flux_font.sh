8600e540a514af5956c6f8bec4dfc569e6365086
David White <david@runonflux.io>
2024-09-03 10:08:55 +0000

    Add Flux specific Font config

---
+++ gen_flux_font.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Generate fbpad fonts
+
+FP="$(pwd)"
+OP="-h18 -w11"
+SZ=8
+
+./mkfn_ft $OP $FP/Flux-Regular.ttf:$SZ		>/usr/lib/flux-r.tf
+#./mkfn_ft $OP $FP/Flux-Italic.ttf:$SZ		>/usr/lib/flux-i.tf
+#./mkfn_ft $OP $FP/Flux-Bold.ttf:$SZ		>/usr/lib/flux-b.tf
+
