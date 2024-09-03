b9cbb013dcfc8c1375a444de133dcd9e26dd9a67
David White <david@runonflux.io>
2024-09-03 10:21:24 +0000

    Generate font in local dir

--- gen_flux_font.sh
+++ gen_flux_font.sh
@@ -5,7 +5,7 @@ FP="$(pwd)"
 OP="-h18 -w11"
 SZ=8
 
-./mkfn_ft $OP $FP/Flux-Regular.ttf:$SZ		>/usr/lib/flux-r.tf
+./mkfn_ft $OP $FP/Flux-Regular.ttf:$SZ		>${FP}/flux-r.tf
 #./mkfn_ft $OP $FP/Flux-Italic.ttf:$SZ		>/usr/lib/flux-i.tf
 #./mkfn_ft $OP $FP/Flux-Bold.ttf:$SZ		>/usr/lib/flux-b.tf
 
